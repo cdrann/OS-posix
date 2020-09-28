66b30329ea4a858c42bf1704d0a81572cd384653
Anna Redko <45363539+cdrann@users.noreply.github.com>
2020-09-28 22:01:08 +0700

    Create main.c

---
+++ lab1/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define ERROR_TREAD_CREATE 1
+
+void print_lines(char *line) {
+    for (int i = 0; i < 10; i++) {
+        printf("%s\n", line);
+    }
+}
+
+void exit_error(int err_code) {
+    char buff[256];
+    strerror_r(err_code, buff, sizeof(buf));
+        
+    fprintf(stderr, "%s: creating thread: %s\n", argv[0], buff);
+
+    exit(ERROR_TREAD_CREATE);
+}
+
+
+void *start_routine(void * param) {
+    print_lines("Child\n");
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int err_code = pthread_create(&thread, NULL, start_routine, NULL);
+
+    if (err_code != SUCCESS) {
+        exit_error(err_code);
+    }
+    
+    print_lines("Parent\n");
+    
+    
+    pthread_exit(NULL);  
+    return (EXIT_SUCCESS);
+}
