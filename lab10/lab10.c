292baf31adbd60d6969039baaa91e94ef701ad2a
Anna Redko <45363539+cdrann@users.noreply.github.com>
2020-10-27 21:42:46 +0700

    Update lab10.c
    added error handler & removed magic const

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -3,86 +3,120 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
-   
+
 pthread_mutex_t mutex_1;
 pthread_mutex_t mutex_2;
 pthread_mutex_t mutex_3;
 
 #define SUCCESS 0
-#define ERROR_TREAD_CREATE 1
 
-void exit_error(int err_code, int err_returns) {
+#define ERROR_TREAD_CREATE 1
+#define ERROR_MUTEXATTR_INIT 2
+#define ERROR_MUTEXATTR_SETTYPE 3
+#define ERROR_MUTEX_INIT 4
+#define ERROR_MUTEX_LOCK 5
+#define ERROR_MUTEX_UNLOCK 6
+#define ERROR_MUTEX_DESTROY 7
+
+#define NUM_PARENT_LINES 10
+#define NUM_CHILD_LINES 10
+
+
+void exit_error(int err_code, char *message, int err_returns) {
     char buff[256];
     strerror_r(err_code, buff, sizeof(buff));
-    fprintf(stderr, "%s: creating thread: %s\n", buff);
+
+    fprintf(stderr, message, buff);
+
     exit(err_returns);
 }
 
+void mutex_lock(pthread_mutex_t* mutex) {
+    int err_code = pthread_mutex_lock(mutex);
+    if(err_code != SUCCESS) {
+        exit_error(err_code, "%s: cannot lock mutex", ERROR_MUTEX_LOCK);
+    }
+}
+
+
+void mutex_unlock(pthread_mutex_t* mutex) {
+    int err_code = pthread_mutex_unlock(mutex);
+    if(err_code != SUCCESS) {
+        exit_error(err_code, "%s: cannot unlock mutex", ERROR_MUTEX_UNLOCK);
+    }
+}
+
+
+void mutex_destroy(pthread_mutex_t* mutex) {
+    int err_code;
+    if((err_code = pthread_mutex_destroy(mutex)) != SUCCESS) {
+        exit_error(err_code, "%s: cannot destroy mutex", ERROR_MUTEX_DESTROY);
+    }
+}
+
+void errorcheck_mutex_init(pthread_mutex_t* mutex) {
+    pthread_mutexattr_t attr_1;
+    int err_code;
+
+    err_code = pthread_mutexattr_init(&attr_1);
+    if(err_code != SUCCESS) {
+        exit_error(err_code, "%s: cannot init mutex attr", ERROR_MUTEXATTR_INIT);
+    }
+
+    err_code = pthread_mutexattr_settype(&attr_1, PTHREAD_MUTEX_ERRORCHECK);
+    if(err_code != SUCCESS) {
+        exit_error(err_code, "%s: cannot set mutex attr type", ERROR_MUTEXATTR_SETTYPE);
+    }
+
+    err_code = pthread_mutex_init(mutex, &attr_1);
+    if(err_code != SUCCESS) {
+        exit_error(err_code, "%s: cannot init mutex", ERROR_MUTEX_INIT);
+    }
+}
+
 void *thread_body(void *param) {
-    pthread_mutex_lock(&mutex_1);
+    mutex_lock(&mutex_1);
 
-    for(int i = 0; i < 5; ++i) {
-        pthread_mutex_lock(&mutex_2);
-
+    for(int i = 0; i < NUM_CHILD_LINES; ++i) {
+        mutex_lock(&mutex_2);
         printf("String %i from child\n", i);
-
-        pthread_mutex_unlock(&mutex_1);
-
-        pthread_mutex_lock(&mutex_3);
-
-        pthread_mutex_unlock(&mutex_2);
-
-        pthread_mutex_lock(&mutex_1);
-
-        pthread_mutex_unlock(&mutex_3);
+        mutex_unlock(&mutex_1);
+        mutex_lock(&mutex_3);
+        mutex_unlock(&mutex_2);
+        mutex_lock(&mutex_1);
+        mutex_unlock(&mutex_3);
     }
 
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    pthread_mutexattr_t attr_1;
-    pthread_mutexattr_t attr_2;
-    pthread_mutexattr_t attr_3;
-
-    pthread_mutexattr_init(&attr_1);
-    pthread_mutexattr_init(&attr_2);
-    pthread_mutexattr_init(&attr_3);
-
-    pthread_mutexattr_settype(&attr_1, PTHREAD_MUTEX_ERRORCHECK);
-    pthread_mutexattr_settype(&attr_2, PTHREAD_MUTEX_ERRORCHECK);
-    pthread_mutexattr_settype(&attr_3, PTHREAD_MUTEX_ERRORCHECK);
-
-    pthread_mutex_init(&mutex_1, &attr_1);
-    pthread_mutex_init(&mutex_2, &attr_2);
-    pthread_mutex_init(&mutex_3, &attr_3);
+    errorcheck_mutex_init(&mutex_1);
+    errorcheck_mutex_init(&mutex_2);
+    errorcheck_mutex_init(&mutex_3);
 
     pthread_t thread;
 
     int err_code = pthread_create(&thread, NULL, thread_body, NULL);
-
     if (err_code != SUCCESS) {
-        exit_error(err_code, ERROR_TREAD_CREATE);
+        exit_error(err_code, "%s: error creating thread", ERROR_TREAD_CREATE);
     }
 
-    pthread_mutex_lock(&mutex_2);
+    mutex_lock(&mutex_2);
 
-    for(int i = 0; i < 5; ++i) {
-        pthread_mutex_lock(&mutex_3);
-
+    for(int i = 0; i < NUM_PARENT_LINES; ++i) {
+        mutex_lock(&mutex_3);
         printf("String %i from parent\n", i);
-
-        pthread_mutex_unlock(&mutex_2);
-        pthread_mutex_lock(&mutex_1);
-
-        pthread_mutex_unlock(&mutex_3);
-
-        pthread_mutex_lock(&mutex_2);
-        pthread_mutex_unlock(&mutex_1);
+        mutex_unlock(&mutex_2);
+        mutex_lock(&mutex_1);
+        mutex_unlock(&mutex_3);
+        mutex_lock(&mutex_2);
+        mutex_unlock(&mutex_1);
     }
-    
-    pthread_mutex_destroy(&mutex_1);
-    pthread_mutex_destroy(&mutex_2);
-    pthread_mutex_destroy(&mutex_3);
-    pthread_exit(NULL);   
+
+    mutex_destroy(&mutex_1);
+    mutex_destroy(&mutex_2);
+    mutex_destroy(&mutex_3);
+
+    pthread_exit(NULL);
 }
